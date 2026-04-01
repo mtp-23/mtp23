40b2b9474be4d52181bf4eb32b45dc2139a77c44
Phung Tien Manh <25022934@vnu.edu.vn>
2026-04-01 11:00:13 +0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        map<int, int> freq;
+        for (int i : nums)
+            freq[i]++;
+        vector<pair<int, int>> check(freq.begin(), freq.end());
+        sort(check.begin(), check.end(), [&](auto& a, auto& b)
+        {
+            return a.second > b.second;
+        });
+        vector<int> ans;
+        for (int i = 0; i < k; i++) ans.push_back(check[i].first);
+        return ans;
+    }
+};
